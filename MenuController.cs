768e8643244faebcf36756dc48accd31422c714d
mYsT-26 <167778732+mYsT-26@users.noreply.github.com>
2025-06-19 16:24:20 +0200

    Update MenuController.cs

--- MenuController.cs
+++ MenuController.cs
@@ -6,14 +6,13 @@ using UnityEngine.InputSystem;
 public class MenuController : MonoBehaviour
 {
     public GameObject menuCanvas;
-    // Start is called before the first frame update
+    
     void Start()
     {
         menuCanvas.SetActive(false);
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Tab))
